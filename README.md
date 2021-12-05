c58ab2f70151d7acff780772668bcdb47c4b8019
Duy Khang <khangemail22@gmail.com>
2021-12-05 23:31:56 +0700

    Finalize

--- README.md
+++ README.md
@@ -1,38 +1,5 @@
-# create-svelte
+Phần mềm được viết với ba thành phần chính như sau:
 
-Everything you need to build a Svelte project, powered by [`create-svelte`](https://github.com/sveltejs/kit/tree/master/packages/create-svelte);
-
-## Creating a project
-
-If you're seeing this, you've probably already done this step. Congrats!
-
-```bash
-# create a new project in the current directory
-npm init svelte@next
-
-# create a new project in my-app
-npm init svelte@next my-app
-```
-
-> Note: the `@next` is temporary
-
-## Developing
-
-Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
-
-```bash
-npm run dev
-
-# or start the server and open the app in a new browser tab
-npm run dev -- --open
-```
-
-## Building
-
-Before creating a production version of your app, install an [adapter](https://kit.svelte.dev/docs#adapters) for your target environment. Then:
-
-```bash
-npm run build
-```
-
-> You can preview the built app with `npm run preview`, regardless of whether you installed an adapter. This should _not_ be used to serve your app in production.
+1. Svelte (https://svelte.dev/) và SvelteKit (https://kit.svelte.dev/): thư viện JavaScript để xây dựng giao diện UI trên web. Thư mục `src`.
+2. Tauri (https://tauri.studio/en/): dùng để ém ứng dụng web thành file .exe chạy được trên máy tính. Phần này chỉ là boilerplate, không cần tùy chỉnh nhiều. Thư mục `src-tauri`.
+3. Phần logic: đây là phần logic cho mạng Petri của chúng ta. Thư mục `src/lib`.
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,8 +13,22 @@
         <h1 class="text-2xl font-bold uppercase tracking-wide">
             Ứng dụng mạng Petri
         </h1>
-        <p>Nhóm: BOT20</p>
-        <div class="grid grid-cols-2 grid-rows-2 gap-4 mt-8">
+        <p class="text-right italic text-sm">Thực hiện bởi: BOT20</p>
+        <p class="mt-8">
+            Petri net hay mạng Petri, là một công cụ đồ họa được sử dụng một
+            cách rộng rãi trong việc phân tích dữ liệu, mô phỏng, mô hình hóa
+            lại quá trình kinh doanh và các kịch bản khác. Đây là một cấu trúc
+            toán học có thể giúp trong việc xây dựng công việc kế hoạch hoặc
+            trình bày dữ liệu trên các hệ thống phức tạp.
+        </p>
+        <p class="mt-4">
+            Ở bài tập lớn lần này, nhóm đã hoàn thành việc trả lời cũng như mô
+            phỏng lại một số mô hình mạng Petri được đề cập đến trong bài tập
+            lớn. Việc mô phỏng lại giúp cho nhóm có cái nhìn rõ và tổng quan hơn
+            về cách mà mạng Petri hoạt động, từ đó cho thấy được sự quan trọng
+            của nó trong các công việc thực tiễn.
+        </p>
+        <div class="grid grid-cols-2 grid-rows-1 gap-4 mt-4 mb-4">
             <Card
                 title="Mạng Petri bác sĩ 🧑‍⚕️"
                 description="Mô hình mạng Petri cho một bác sĩ trong phòng khám"
@@ -29,13 +43,33 @@
                 alt="Bệnh nhân xếp hàng"
                 href="/demo/2"
             />
-            <Card
-                title="Mạng Petri tổng hợp 🏥"
-                description="Mô hình mạng Petri tổng hợp cho quá trình khám bệnh"
-                imgSrc="/chua-benh.jpg"
-                alt="Khám bệnh COVID"
-                href="/demo/3"
-            />
+        </div>
+        <Card
+            title="Mạng Petri tổng hợp 🏥"
+            description="Mô hình mạng Petri tổng hợp cho quá trình khám bệnh"
+            imgSrc="/chua-benh.jpg"
+            alt="Khám bệnh COVID"
+            href="/demo/3"
+        />
+
+        <p class="mt-4 p-2 bg-blue-50 text-blue-900 rounded-md shadow-md text-justify">
+            Qua bài tập lớn lần này, nhóm đã tìm hiểu và biết thêm rất nhiều
+            kiến thức xoay quanh mạng Petri. Nhóm chúng em muốn gửi lời cảm ơn
+            chân thành nhất đến các thầy cô phụ trách bộ môn khi đã cung cấp rất
+            nhiều kiến thức cho nhóm có thể hoàn thành được bài tập lớn lần này
+            và có thể hiểu thêm được một loại mô hình vẫn còn rất mới
+            mẻ nhưng lại cực kì hữu ích trong cuộc sống thực tiễn!
+        </p>
+
+        <div class="text-right text-sm mt-4 border-t pt-2">
+            <p class="italic">Thành viên nhóm:</p>
+            <ul>
+                <li>Nguyễn Tấn Phát</li>
+                <li>Nguyễn Duy Khang</li>
+                <li>Nguyễn Vĩnh Sang</li>
+                <li>Nguyễn Phúc Bình Khang</li>
+                <li>Trần Thanh Hải</li>
+            </ul>
         </div>
     </div>
 </main>
